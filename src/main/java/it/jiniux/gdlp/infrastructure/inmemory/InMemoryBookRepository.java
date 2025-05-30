c18c02d9adc403e4b81b8e84089a77c28ebd1ec9
jiniux <53437303+jiniux@users.noreply.github.com>
2025-05-30 21:45:35 +0200

    feat: add InMemoryBookRepository to core package

---
+++ src/main/java/it/jiniux/gdlp/infrastructure/inmemory/InMemoryBookRepository.java
@@ -0,0 +1,73 @@
+package it.jiniux.gdlp.infrastructure.inmemory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import it.jiniux.gdlp.domain.Author;
+import it.jiniux.gdlp.domain.Book;
+import it.jiniux.gdlp.domain.BookRepository;
+import it.jiniux.gdlp.domain.Book.Title;
+import it.jiniux.gdlp.domain.Isbn;
+
+public class InMemoryBookRepository implements BookRepository {
+    private final List<Book> books = new ArrayList<>();
+    private final Map<Isbn, Book> isbnIndex = new HashMap<>();
+    
+    @Override
+    public void saveBook(Book book) {
+        Optional<Book> existingBook = findBookByTitleAndAuthors(book.getTitle(), book.getAuthors());
+        existingBook.ifPresent(books::remove);
+
+        for (Isbn isbn : book.getIsbns()) {
+            isbnIndex.put(isbn, book);
+        }
+        
+        books.add(book);
+    }
+
+    @Override
+    public void deleteBook(Book book) {
+        books.removeIf(b -> 
+            b.getTitle().equals(book.getTitle()) && 
+            b.getAuthors().equals(book.getAuthors()));
+
+        for (Isbn isbn : book.getIsbns()) {
+            isbnIndex.remove(isbn);
+        }
+    }
+
+    @Override
+    public Set<Isbn> findAlreadyExistingIsbns(List<Isbn> isbns) {
+        Set<Isbn> existingIsbns = new HashSet<>();
+        
+        for (Book book : books) {
+            Set<Isbn> bookIsbns = book.getIsbns();
+            for (Isbn isbn : isbns) {
+                if (bookIsbns.contains(isbn)) {
+                    existingIsbns.add(isbn);
+                }
+            }
+        }
+        
+        return existingIsbns;
+    }
+
+    @Override
+    public Optional<Book> findBookByTitleAndAuthors(Title title, Set<Author> authors) {
+        return books.stream()
+            .filter(book -> book.getTitle().equals(title) && book.getAuthors().equals(authors))
+            .findFirst();
+    }
+
+    @Override
+    public Optional<Book> findBookByIsbn(Isbn isbn) {
+        return isbnIndex.containsKey(isbn) ? 
+            Optional.of(isbnIndex.get(isbn)) : 
+            books.stream().filter(book -> book.getIsbns().contains(isbn)).findFirst();
+    }
+}
